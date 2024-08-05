a1005fa0fbaa14107064218c51cfec944168ad36
eskimo204 <99628010+eskimo204@users.noreply.github.com>
2024-08-05 13:56:26 +0900

    import문 변경

--- New_Streamlit.py
+++ New_Streamlit.py
@@ -10,7 +10,8 @@ from PIL import Image
 from io import BytesIO
 from unstructured.partition.pdf import partition_pdf
 from langchain.text_splitter import CharacterTextSplitter
-from langchain.vectorstores import FAISS
+# from langchain.vectorstores import FAISS
+from langchain.embeddings import FAISS
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.chains.question_answering import load_qa_chain
 from langchain.llms import OpenAI
