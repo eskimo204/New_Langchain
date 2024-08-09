08dd66cca8679397b80f94cf12038426d3c01b3d
eskimo204 <99628010+eskimo204@users.noreply.github.com>
2024-08-09 19:08:57 +0900

    빼먹은 부분 수정

--- New_Streamlit.py
+++ New_Streamlit.py
@@ -301,6 +301,10 @@ if uploaded_file and api_key:
     # PDF 파일의 요소들을 추출
     raw_pdf_elements, image_output_dir = extract_pdf_elements(os.path.dirname(temp_file_path), fname)
 
+    # 이미지가 저장된 디렉토리의 파일 목록을 확인합니다.
+    image_files = list_directory_contents(image_output_dir)
+    st.write(f"디렉토리 {image_output_dir}의 파일 목록: {image_files}")
+
     #이미지가 저장된 경로를 출력
     st.write(f"이미지가 저장된 경로: {image_output_dir}")
 
