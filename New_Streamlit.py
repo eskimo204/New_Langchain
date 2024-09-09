c98735c117f46eae58452436e9b788caf8aed98e
eskimo204 <99628010+eskimo204@users.noreply.github.com>
2024-09-09 21:15:57 +0900

    Update New_Streamlit.py

--- New_Streamlit.py
+++ New_Streamlit.py
@@ -15,7 +15,7 @@ import shutil
 import nltk
 nltk.download('punkt')
 
-#from PIL import Image
+from PIL import Image
 import PIL.Image as pil
 from io import BytesIO
 from unstructured.partition.pdf import partition_pdf
